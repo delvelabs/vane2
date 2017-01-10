29b9808cac1856280b1543fabb11b25e2bede155
Nicolas <nicolas.aubry@delvelabs.ca>
2017-01-10 15:04:37 -0500

    Add basic plugin search in a html page.

---
+++ tests/passivepluginsfinder_tests.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from os.path import join, dirname
+from vane.passivepluginsfinder import PassivePluginsFinder
+from unittest.mock import MagicMock
+
+
+class TestPassivePluginsFinder(TestCase):
+
+    def test_list_plugins_find_official_plugin_references_in_page_source(self):
+        sample_page_file_name = join(dirname(__file__), "samples/delvelabs_homepage.html")
+        plugins_finder = PassivePluginsFinder()
+        plugins_database = MagicMock()
+        plugins_database.get_plugins.return_value = ["disqus-comment-system", "mobile-navigation"]
+        plugins_finder.set_plugins_database(plugins_database)
+
+        plugins = plugins_finder.list_plugins(sample_page_file_name)
+
+        self.assertIn("disqus-comment-system", plugins)
+        self.assertIn("mobile-navigation", plugins)
+
+    def test_plugin_names_equal_ignore_case(self):
+        name0 = "Mobile-Navigation"
+        name1 = "mobile-navigation"
+        plugins_finder = PassivePluginsFinder()
+
+        self.assertTrue(plugins_finder.plugin_names_equal(name0, name1))
+
+    def test_plugin_names_equal_ignore_whitespace(self):
+        name0 = "mobile navigation"
+        name1 = "mobile-navigation"
+        plugins_finder = PassivePluginsFinder()
+
+        self.assertTrue(plugins_finder.plugin_names_equal(name0, name1))
+
+    def test_plugin_names_equal_ignore_hyphens(self):
+        name0 = "mobilenavigation"
+        name1 = "mobile-navigation"
+        plugins_finder = PassivePluginsFinder()
+
+        self.assertTrue(plugins_finder.plugin_names_equal(name0, name1))
---
+++ vane/passivepluginsfinder.py
@@ -0,0 +1,67 @@
+from lxml import etree
+import re
+
+
+plugins_url = re.compile("https?://www\.[^.]+\.[^/]+/wp-content/plugins/")
+
+
+class PassivePluginsFinder:
+
+    def __init__(self):
+        self.plugins_database = None
+
+    def set_plugins_database(self, database):
+        self.plugins_database = database
+
+    def list_plugins(self, html_page):
+        plugins = []
+        html_tree_iterator = etree.iterparse(html_page, html=True)
+        plugins.extend(self._search_elements(html_tree_iterator))
+        return plugins
+
+    def _search_elements(self, html_tree_iterator):
+        plugins = []
+        for action, element in html_tree_iterator:
+            plugins.extend(self._search_in_element_attributes(element))
+        return plugins
+
+    def _search_in_element_attributes(self, element):
+        plugins = []
+        for attribute_name, attribute_value in element.items():
+            if "plugin" in attribute_name:
+                if self.is_plugin(attribute_value):
+                    plugins.append(self.get_official_plugin_name(attribute_value))
+            elif self._is_plugin_url(attribute_value):
+                plugins.append(self._get_plugin_name_from_plugins_url(attribute_value))
+        return plugins
+
+    def is_plugin(self, plugin_name):
+        for plugin in self.plugins_database.get_plugins():
+            if self.plugin_names_equal(plugin, plugin_name):
+                return True
+        return False
+
+    def get_official_plugin_name(self, plugin_name):
+        for plugin in self.plugins_database.get_plugins():
+            if self.plugin_names_equal(plugin, plugin_name):
+                return plugin
+
+    def plugin_names_equal(self, name0, name1):
+        return self._remove_hyphens(self._normalize_plugin_names(name0)) == \
+               self._remove_hyphens(self._normalize_plugin_names(name1))
+
+    def _remove_hyphens(self, name):
+        return re.sub("-", "", name)
+
+    def _normalize_plugin_names(self, name):
+        normalized_name = name.lower()
+        return re.sub(" ", "-", normalized_name)
+
+    def _is_plugin_url(self, url):
+        return plugins_url.search(url)
+
+    def _get_plugin_name_from_plugins_url(self, url):
+        plugin_name = plugins_url.sub("", url)
+        plugin_name = re.match("[^/]+", plugin_name).group(0)
+        return plugin_name
+
