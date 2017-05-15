d6d2cdc2477519672c14aa7445868e269b63b461
nicolas <nicolas.aubry@delvelabs.ca>
2017-05-15 10:39:39 -0400

    Remove useless fixtures.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -15,8 +15,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from collections import OrderedDict
-
 from unittest.mock import MagicMock
 
 import asyncio
@@ -24,35 +22,6 @@ from functools import wraps
 from aiohttp.test_utils import loop_context
 
 from easyinject import Injector
-
-
-def wrap_lists_in_unordered_lists(iterable):
-    """Recursively iterate over the contents of a iterable and wrap all lists elements into UnorderedList"""
-    if type(iterable) == dict or type(iterable) == OrderedDict:
-        for key, value in iterable.items():
-            iterable[key] = wrap_lists_in_unordered_lists(value)
-        return iterable
-    elif type(iterable) == list:
-        li = [wrap_lists_in_unordered_lists(element) for element in iterable]
-        return UnorderedList(li)
-    else:
-        return iterable
-
-
-class UnorderedList:
-    """Wrapper for a list, used for equality assertion based on orderless elements occurrence."""
-
-    def __init__(self, list):
-        self.list = list
-
-    def __eq__(self, other):
-        li = list(self.list)
-        try:
-            for element in other:
-                li.remove(element)
-        except ValueError:
-            return False
-        return len(li) == 0
 
 
 def html_file_to_hammertime_response(filename):
