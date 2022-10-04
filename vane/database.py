1426913e5eace001abc7c0895c782dac3925c629
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 02:17:07 +0000

    Adding numeric_owner as keyword arguement

--- vane/database.py
+++ vane/database.py
@@ -143,7 +143,7 @@ class Database:
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(archive, re.sub("\.tar\.gz$","",archive_filename))
